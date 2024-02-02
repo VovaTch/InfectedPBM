Need to fix the RVQ training, because probably the gradients don't beyond the 1st codebook. Also, cleared the notebook

models/multi_level_vqvae/blocks.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from common import registry
-from ..vq_codebook import VQCodebook
+from ..vq_codebook import ResidualCodebookCollection, VQCodebook
 
 
 class Res1DBlock(nn.Module):
@@ -175,16 +175,21 @@ class VQ1D(nn.Module):
         dict: A dictionary containing the output of the forward pass.
     """
 
-    def __init__(self, token_dim: int, num_tokens: int = 8192) -> None:
+    def __init__(
+        self, token_dim: int, num_tokens: int = 8192, num_codebooks: int = 1
+    ) -> None:
         """
         Initialize the Block class.
 
         Args:
             token_dim (int): The dimensionality of each token.
             num_tokens (int, optional): The number of tokens in the codebook. Defaults to 8192.
+            num_codebooks (int, optional): The number of codebooks to use. Defaults to 1.
         """
         super().__init__()
-        self.vq_codebook = VQCodebook(token_dim, num_tokens=num_tokens)
+        self.vq_codebook = ResidualCodebookCollection(
+            token_dim, num_codebooks=num_codebooks, num_tokens=num_tokens
+        )
 
     def forward(
         self, z_e: torch.Tensor, extract_losses: bool = False

models/vq_codebook.py
@@ -85,7 +85,7 @@ class VQCodebook(nn.Module):
     def reset_usage(self) -> None:
         self.usage.zero_()  # reset usage between epochs # type: ignore
 
-    def random_restart(self) -> int:
+    def random_restart(self) -> float:
         #  randomly restart all dead codes below threshold with random code in codebook
         dead_codes = torch.nonzero(self.usage < self.usage_threshold).squeeze(1)  # type: ignore
         # used_codes = torch.nonzero(self.usage >= self.usage_threshold).squeeze(1)
@@ -168,10 +168,10 @@ def vq_codebook_select(x_in: torch.Tensor, emb_batch: torch.Tensor):
 class ResidualCodebookCollection(nn.Module):
     def __init__(self, token_dim: int, num_codebooks: int, num_tokens: int) -> None:
         super().__init__()
-        _codebook_list = [
-            VQCodebook(token_dim, num_tokens=num_tokens) for _ in range(num_codebooks)
-        ]
-        self.vq_codebooks = nn.ModuleList(*_codebook_list)
+        self.vq_codebooks = nn.ModuleList(
+            [VQCodebook(token_dim, num_tokens=num_tokens) for _ in range(num_codebooks)]
+        )
+        self.token_dim = token_dim
 
     def apply_codebook(
         self, x_in: torch.Tensor, code_sg: bool = False
@@ -198,7 +198,7 @@ class ResidualCodebookCollection(nn.Module):
             z_q += z_q_ind
             indices.append(indices_ind)
 
-        indices = torch.stack(indices, dim=-2)
+        indices = torch.cat(indices, dim=-1)
         return z_q, indices
 
     def embed_codebook(self, indices: torch.Tensor) -> torch.Tensor:
@@ -212,13 +212,13 @@ class ResidualCodebookCollection(nn.Module):
             torch.Tensor: The embedded codebook.
 
         """
+        emb = torch.zeros((indices.shape[0], indices.shape[1], self.token_dim)).to(
+            indices.device
+        )
         for idx, codebook in enumerate(self.vq_codebooks):
-            if idx == 0:
-                emb = codebook.embed_codebook(indices[..., idx])
-            else:
-                emb += codebook.embed_codebook(indices[..., idx])
+            emb += codebook.embed_codebook(indices[..., idx])
 
-        return emb
+        return emb.transpose(1, 2)
 
     def update_usage(self, min_enc: torch.Tensor) -> None:
         """
@@ -240,14 +240,14 @@ class ResidualCodebookCollection(nn.Module):
         for codebook in self.vq_codebooks:
             codebook.reset_usage()
 
-    def random_restart(self) -> int:
+    def random_restart(self) -> float:
         """
         Performs a random restart for the optimization algorithm.
 
         Returns:
-            int: The number of dead codes
+            float: The average number of dead codes
         """
         dead_codes = 0
         for codebook in self.vq_codebooks:
             dead_codes += codebook.random_restart()
-        return dead_codes
+        return dead_codes / len(self.vq_codebooks)