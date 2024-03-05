c7882384d856c426efd8ae7985a2db2b5cc610cb
Vladimir Tchuiev <rmcf126@gmail.com>
2024-03-05 20:32:34 +0200

    Added a test to see if the RVQ model works as expected for larger slice sizes

--- tests/test_models/test_rvq.py
+++ tests/test_models/test_rvq.py
@@ -12,20 +12,27 @@ def codebook_collection():
 
 
 def test_apply_codebook(codebook_collection: ResidualCodebookCollection) -> None:
-    x_in = torch.randn(10, 64, 2)
+    x_in = torch.randn(10, 64, 2)  # BS x Cnl x slice_len
     z_q, indices = codebook_collection.apply_codebook(x_in)
-    assert z_q.shape == x_in.shape
-    assert indices.shape == (
-        10,
-        2,
+    assert z_q.shape == (
+        x_in.shape[0],
         4,
-    )
+        x_in.shape[1],
+        x_in.shape[2],
+    )  # BS x num_codebooks x Cnl x slice_len
+    assert indices.shape == (10, 2, 4)  # BS x slice_len x num_codebooks
 
 
 def test_embed_codebook(codebook_collection: ResidualCodebookCollection) -> None:
-    indices = torch.randint(64, size=(10, 2, 4))
+    indices = torch.randint(64, size=(10, 2, 4))  # BS x slice_len x num_codebooks
     emb = codebook_collection.embed_codebook(indices)
-    assert emb.shape == (10, 64, 2)
+    assert emb.shape == (10, 64, 2)  # BS x Cnl x slice_len
+
+
+def test_embed_codebook_large(codebook_collection: ResidualCodebookCollection) -> None:
+    indices = torch.randint(256, size=(256, 4, 4))
+    emb = codebook_collection.embed_codebook(indices)
+    assert emb.shape == (256, 64, 4)
 
 
 def test_update_usage(codebook_collection: ResidualCodebookCollection) -> None:
@@ -40,5 +47,5 @@ def test_reset_usage(codebook_collection: ResidualCodebookCollection) -> None:
 
 
 def test_random_restart(codebook_collection: ResidualCodebookCollection) -> None:
-    dead_codes = codebook_collection.random_restart()
+    _ = codebook_collection.random_restart()
     # Add assertions here to check if the dead codes are counted correctly
